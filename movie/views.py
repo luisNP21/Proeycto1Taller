87ca430c24b806537c20e8190ec8470d7371497c
Luis <laneriop@eafit.edu.co>
2025-02-05 11:41:35 -0500

    first commit

---
+++ movie/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {'name': 'Greg Lim'})
+
+def about(request):
+    return HttpResponse('Welcome to about page</h1>')
+
+
